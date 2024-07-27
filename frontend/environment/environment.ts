65cae33afb56ff6270e02d8d573ca9a9938eac82
Oscar Arley Sanchez <oa.sanchez2@uniandes.edu.co>
2024-07-26 19:51:36 -0500

    front

---
+++ frontend/environment/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    backendHost : "http://localhost:8085/api",
+    aws : "https://dmtlenb3j0.execute-api.us-east-1.amazonaws.com/api/api"
+  };
